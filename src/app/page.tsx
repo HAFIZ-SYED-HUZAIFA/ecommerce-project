7c506ebfb42bd2cf0effde34e6861bcf8904c059
HAFIZ SYED HUZAIFA <hafizsyedhuzaifa5@gmail.com>
2024-12-21 04:15:45 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+import Discount from '@/components/Discount'
+import Featureproduct from '@/components/Featureproduct'
+import Hero from '@/components/Hero'
+import Latestblog from '@/components/Latestblog'
+import Latestproduct from '@/components/Latestproduct'
+import Offer from '@/components/Offer'
+import Topcategory from '@/components/Topcategory'
+import Trendingproduct from '@/components/Trendingproduct'
+import Uniquefeature from '@/components/Uniquefeature'
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Hero/>
+      <Featureproduct/>
+      <Latestproduct/>
+      <Offer/>
+      <Uniquefeature/>
+      <Trendingproduct/>
+      <Discount/>
+      <Topcategory/>
+      <Latestblog/>
+    </div>
+  )
+}
+
+export default page
+
